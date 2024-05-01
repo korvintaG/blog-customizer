38a99caea280c1958b4d5b7c2d33df2010094705
Sergey Korobok <korvin@kati.ru>
2024-05-01 11:19:43 +0300

    first version for review

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,152 @@
-import { ArrowButton } from 'components/arrow-button';
-import { Button } from 'components/button';
-
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import clsx from 'clsx';
+import { ArrowButton } from 'components/arrow-button';
+import { Button } from 'components/button';
+import { useState, useRef, SyntheticEvent } from 'react';
+import { Text } from 'components/text';
+import { Select } from 'components/select';
+import {
+	ArticleStateType,
+	defaultArticleState,
+	OptionType,
+	fontSizeOptions,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'components/radio-group';
+import { Separator } from 'components/separator';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
+
+/**
+ * Тип пропсов компонента ArticleParamsForm
+ */
+export type ArticleParamsFormProps = {
+	onSet: (newSet: ArticleStateType) => void; // при смене настроек
+	currentState: ArticleStateType; // начальное состояние
+};
+
+/**
+ * Компонент панели задания условий ArticleParamsForm
+ * @onSet - функция смены набора состояний
+ * @currentState - текущее состояние всего блога
+ */
+export const ArticleParamsForm = ({
+	onSet,
+	currentState,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setOpen] = useState(false); // Открытие-закрытие формы
+
+	// для полей формы
+	const [fontFamilyOption, setFontFamilyOption] = useState<OptionType>(
+		currentState.fontFamilyOption
+	);
+	const [fontSizeOption, setFontSizeOption] = useState<OptionType>(
+		currentState.fontSizeOption
+	);
+	const [fontColor, setFontColor] = useState<OptionType>(
+		currentState.fontColor
+	);
+	const [backgroundColor, setBackgroundColor] = useState<OptionType>(
+		currentState.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState<OptionType>(
+		currentState.contentWidth
+	);
+
+	const rootRef = useRef<HTMLDivElement | null>(null); // для хука Ref
+
+	// запуск пользовательского хука для отлавливания клика за пределами сайдбара
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onChange: setOpen,
+		eventName: 'mousedown', // устанавливаем другое название события, чтобы не конфликтовало с компонентой Select
+	});
+
+	/**
+	 * Обработка нажатия кнопки <Применить>
+	 * @param e - SyntheticEvent - стандартный тип переменной обработчика события
+	 */
+	const handleSubmit = (e: SyntheticEvent): void => {
+		e.preventDefault();
+		onSet({
+			fontFamilyOption,
+			fontSizeOption,
+			fontColor,
+			backgroundColor,
+			contentWidth,
+		});
+	};
+
+	/**
+	 * Обработка нажатия кнопки <Сбросить>
+	 * @param e - SyntheticEvent - стандартный тип переменной обработчика события
+	 */
+	const handleReset = (e: SyntheticEvent): void => {
+		e.preventDefault();
+		setFontFamilyOption(defaultArticleState.fontFamilyOption);
+		setFontSizeOption(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+		onSet(defaultArticleState);
+	};
+
+	/**
+	 * Функция переключения открытости сайдбара
+	 */
+	const toggleOpen = (): void => setOpen(!isOpen);
+
 	return (
-		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={rootRef}>
+			{' '}
+			{/* сделали div, чтобы работал клик за пределами модального окна */}
+			<ArrowButton onClick={toggleOpen} isOpen={isOpen} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text as='h2' uppercase size={31} weight={800}>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифты'
+						options={fontFamilyOptions}
+						selected={fontFamilyOption}
+						onChange={setFontFamilyOption}></Select>
+					<RadioGroup
+						options={fontSizeOptions}
+						name='FontSize'
+						title='Размер шрифта'
+						selected={fontSizeOption}
+						onChange={setFontSizeOption}></RadioGroup>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={fontColor}
+						onChange={setFontColor}></Select>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={backgroundColor}
+						onChange={setBackgroundColor}></Select>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={contentWidth}
+						onChange={setContentWidth}></Select>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { useState, StrictMode, CSSProperties } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +16,22 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [ArticleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': ArticleState.fontFamilyOption.value,
+					'--font-size': ArticleState.fontSizeOption.value,
+					'--font-color': ArticleState.fontColor.value,
+					'--container-width': ArticleState.contentWidth.value,
+					'--bg-color': ArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onSet={setArticleState} currentState={ArticleState} />
 			<Article />
 		</div>
 	);
